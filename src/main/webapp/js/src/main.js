f25e5905fb4ed828965b3448db5fa7d3d10db9d6
zhbsbccg <zhbsbccg@outlook.com>
2017-06-27 22:40:59 +0800

    add new file

---
+++ src/main/webapp/js/src/main.js
@@ -0,0 +1,17 @@
+/**
+ * 主要js内容
+ */
+
+import Vue from 'vue'
+import Router from 'vue-router'
+import setPassword from './page/setPassword.vue'
+
+Vue.use(Router)
+
+// routing
+const router = new Router(
+	routes: [
+		
+	]
+);
+
---
+++ src/main/webapp/js/src/page/setPassword.vue
@@ -0,0 +1,9 @@
+<template>
+<div>这是修改密码页面</div>
+</template>
+
+<script>
+export default {
+	name: 'setPassword'
+}
+</script>
